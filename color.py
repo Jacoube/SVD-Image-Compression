42b489e5109595bc037ebb5b9b30d2ff56d0326e
Jacoube <jacobbalek35@gmail.com>
2023-12-13 22:06:27 -0500

    Final version
    The color file has been fixed, the test file has been removed, and README has been added.

--- color.py
+++ color.py
@@ -1,58 +1,23 @@
 from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-import os
 plt.rcParams['figure.figsize'] = [16, 8]
 
+# Load image
 A = imread('comet_crop.jpg')
-X = np.mean(A, -1)  # Convert RGB to grayscale
+print(A)  # Print the image matrix
 
-# 0 - red; 1 - green; 2 - blue
-# Extract only the color channel
+# Show original image
+img = plt.imshow(A)
+plt.axis('off')
+plt.show()
+
+# Extract individual color channels
 red_channel = A[:, :, 0]
 green_channel = A[:, :, 1]
 blue_channel = A[:, :, 2]
 
-# Create an all-zero image with the same shape as the original image
-red_image = np.zeros_like(A)
-green_image = np.zeros_like(A)
-blue_image = np.zeros_like(A)
-
-# Set the red channel of the new image to the extracted red channel values
-red_image[:, :, 0] = red_channel
-green_image[:, :, 1] = green_channel
-blue_image[:, :, 2] = blue_channel
-
-# Show the images
-img = plt.imshow(A)
-img.set_cmap('gray')
-plt.axis('off')
-plt.show()
-
-img = plt.imshow(red_image)
-img.set_cmap('gray')
-plt.axis('off')
-plt.show()
-
-img = plt.imshow(green_image)
-img.set_cmap('gray')
-plt.axis('off')
-plt.show()
-
-img = plt.imshow(blue_image)
-img.set_cmap('gray')
-plt.axis('off')
-plt.show()
-
-# Combine again
-X = np.dstack((red_channel, green_channel, blue_channel))
-img = plt.imshow(X)
-plt.axis('off')
-plt.title('color')
-plt.show()
-
-# SVD for all colors
+# Perform SVD on each color channel
 UR, SR, VTR = np.linalg.svd(red_channel, full_matrices=False)
 SR = np.diag(SR)
 
@@ -63,19 +28,23 @@ UB, SB, VTB = np.linalg.svd(blue_channel, full_matrices=False)
 SB = np.diag(SB)
 
 j = 0
-for r in (20, 50):
-    # Construct approximate image
+for r in (1, 2, 3, 5, 20, 100):
+    # Construct approximate images for each color channel
     Rapprox = UR[:, :r] @ SR[0:r, :r] @ VTR[:r, :]
     Gapprox = UG[:, :r] @ SG[0:r, :r] @ VTG[:r, :]
     Bapprox = UB[:, :r] @ SB[0:r, :r] @ VTB[:r, :]
 
-    Xapprox = np.stack((Rapprox, Gapprox, Bapprox), axis=-1)
+    # Normalize the reconstructed channels to the [0, 255] range
+    Rapprox = np.clip(Rapprox, 0, 255).astype(np.uint8)
+    Gapprox = np.clip(Gapprox, 0, 255).astype(np.uint8)
+    Bapprox = np.clip(Bapprox, 0, 255).astype(np.uint8)
 
-    img = plt.imshow(Xapprox)
-    img.set_cmap('gray')
+    # Merge the color channels back into RGB image
+    Xapprox = np.dstack((Rapprox, Gapprox, Bapprox))
+
+    # Display reconstructed images
+    plt.imshow(Xapprox)
     plt.axis('off')
     plt.title('r =' + str(r))
     plt.show()
-
-print("done")
 
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 plt.rcParams['figure.figsize'] = [16, 8]
 
+# Load image
 A = imread('comet_crop.jpg')
 X = np.mean(A, -1)  # Convert RGB to grayscale
+print(X)  # Print the image matrix
 
-img = plt.imshow(A)
+
+img = plt.imshow(X)
 img.set_cmap('gray')
 plt.axis('off')
 plt.show()
 
+# SVD
 U, S, VT = np.linalg.svd(X, full_matrices=False)
 S = np.diag(S)
+print("U:", U)  # Print the SVD components
+print("S:", S)
+print("VT:", VT)
+
+print(len(A), len(A[0]))  # Find sizes of matrices
+print(len(U), len(U[0]))
+print(len(S), len(S[0]))
+print(len(VT), len(VT[0]))
 
 j = 0
 for r in (1, 2, 3, 5, 20, 100):
@@ -21,10 +32,15 @@ for r in (1, 2, 3, 5, 20, 100):
     Xapprox = U[:, :r] @ S[0:r, :r] @ VT[:r, :]
     plt.figure(j+1)
     j += 1
+    # Display image
     img = plt.imshow(Xapprox)
     img.set_cmap('gray')
     plt.axis('off')
     plt.title('r =' + str(r))
     plt.show()
+    print("r=", r, ": ")  # Find sizes of Xapprox
+    print("U:", len(U[:, :r]), " x ", len(U[:, :r][0]))
+    print("S:", len(S[0:r, :r]), " x ", len(S[0:r, :r][0]))
+    print("VT:", len(VT[:r, :]), " x ", len(VT[:r, :][0]))
 
 print("done")
